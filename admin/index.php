fa6c5234ec37b46ff58afde61a922c43f71a9177
robbieveivers <robbieveivers@outlook.com>
2021-01-18 14:07:31 +1000

    first commit

---
+++ admin/index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title> Food ordering website Roberto's - Home Page</title>
+    </head>
+    
+    <body>
+        <!-- Menu section starts -->
+        <div class="menu">
+            Menu goes here
+        </div>
+        <!-- Menu section ends  -->
+
+        <!-- Main content section starts  -->
+        <div class="main-content">
+            Main content goes here
+        </div>
+        <!-- Main content section ends  -->
+
+        <!-- Footer section starts  -->
+        <div class="footer">
+            footer goes here
+        </div>
+        <!-- Footer section ends  -->
+    </body>
+
+</html>
