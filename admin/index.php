5882cd04e0f95cc589410c3e3c332521b729297c
Robert Veivers <56959826+robbieveivers@users.noreply.github.com>
2021-02-04 13:25:36 +1000

    Basic Admin Panel Created

--- admin/index.php
+++ admin/index.php
@@ -1,24 +1,34 @@
 <html>
     <head>
         <title> Food ordering website Roberto's - Home Page</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
     </head>
     
     <body>
         <!-- Menu section starts -->
         <div class="menu">
-            Menu goes here
+            <div class="wrapper"> 
+                Menu goes here
+            </div>
         </div>
         <!-- Menu section ends  -->
 
         <!-- Main content section starts  -->
         <div class="main-content">
-            Main content goes here
+            <div class="wrapper"> 
+                Main content goes here
+            </div>
+            
         </div>
         <!-- Main content section ends  -->
 
         <!-- Footer section starts  -->
         <div class="footer">
-            footer goes here
+            <div class="wrapper"> 
+                footer goes here
+            </div>
+            
         </div>
         <!-- Footer section ends  -->
     </body>
