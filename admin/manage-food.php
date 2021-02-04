3d073d57b5988a6db8ed5ede44dbdd75bf7f72eb
Robert Veivers <56959826+robbieveivers@users.noreply.github.com>
2021-02-04 14:26:35 +1000

    Dashboard Complete

---
+++ admin/manage-food.php
@@ -0,0 +1,12 @@
+<?php include('partials/menu.php'); ?>
+
+        <!-- Main content section starts  -->
+        <div class="main-content">
+            <div class="wrapper"> 
+                <h1>Manage Food</h1>
+
+            </div>
+        </div>
+        <!-- Main content section ends  -->
+
+<?php include('partials/footer.php'); ?>
