b67fcfcc451b39660fb6d78f5aca02636c1b11d8
Robert Veivers <56959826+robbieveivers@users.noreply.github.com>
2021-02-08 14:54:54 +1000

    Authentication and Access Control

--- admin/index.php
+++ admin/index.php
@@ -3,7 +3,16 @@
         <div class="main-content">
             <div class="wrapper"> 
                 <h1>Dashboard</h1>
+                <br><br>
+                <?php 
+                //Check whether there is a login message
+                if(isset($_SESSION['login'])){
+                    echo $_SESSION['login'];
+                    unset($_SESSION['login']);
+                }
 
+                ?>
+                <br><br>
                 <div class="col-4 text-center">
                     <h1>5</h1>
                     <br>
--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -1,4 +1,9 @@
-<?php include('../config/constants.php'); ?>
+<?php 
+    include('../config/constants.php');
+    include('login-check.php');
+?>
+
+
 <html>
     <head>
         <title> Food ordering website Roberto's - Home Page</title>
@@ -16,6 +21,7 @@
                     <li><a href="manage-category.php">Category</a></li>
                     <li><a href="manage-food.php">Food</a></li>
                     <li><a href="manage-order.php">Order</a></li>
+                    <li><a href="logout.php">Logout</a></li>
                 </ul>
             </div>
         </div>
